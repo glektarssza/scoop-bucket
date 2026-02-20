c54718297a94578014a67aa6bc41012ae9f862d9
G'lek Tarssza <glek@glektarssza.com>
2026-02-20 16:00:47 -0700

    Custom formatting script.

---
+++ scripts/formatjson.ps1
@@ -0,0 +1,48 @@
+<#
+.SYNOPSIS
+    Format manifest.
+.PARAMETER App
+    Manifest to format.
+
+    Wildcards are supported.
+.PARAMETER Dir
+    Where to search for manifest(s).
+.EXAMPLE
+    PS BUCKETROOT> .\bin\formatjson.ps1
+    Format all manifests inside bucket directory.
+.EXAMPLE
+    PS BUCKETROOT> .\bin\formatjson.ps1 7zip
+    Format manifest '7zip' inside bucket directory.
+#>
+param(
+    [String] $App = '*',
+    [Parameter(Mandatory = $true)]
+    [ValidateScript( {
+        if (!(Test-Path $_ -Type Container)) {
+            throw "$_ is not a directory!"
+        } else {
+            $true
+        }
+    })]
+    [String] $Dir
+)
+
+Write-Debug "Loading scoop libs..."
+
+. "$env:SCOOP_HOME\lib\core.ps1"
+. "$env:SCOOP_HOME\lib\manifest.ps1"
+. "$env:SCOOP_HOME\lib\json.ps1"
+
+$Dir = Convert-Path $Dir
+
+Get-ChildItem $Dir -Filter "$App.json" -Recurse | ForEach-Object {
+    $file = $_.FullName
+    Write-Debug "Processing `"$file`"..."
+    # beautify
+    $json = parse_json $file | ConvertToPrettyJson
+    # convert crlf to lf
+    $json = $json -replace "`r`n", "`n"
+    # convert to 4 spaces
+    $json = $json -replace "`t", '    '
+    [System.IO.File]::WriteAllLines($file, $json)
+}
